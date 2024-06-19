745d16be293f45c77810add4e1222111b0daa121
Mat <71132805+mat512@users.noreply.github.com>
2024-06-19 11:08:22 +0200

    feat(server): add user filter

--- back/src/users/users.resolver.ts
+++ back/src/users/users.resolver.ts
@@ -1,14 +1,15 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { FilterUserInput } from './dto/filter-user.input';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  findAll() {
-    return this.usersService.findAll();
+  findAll(@Args('filter', { nullable: true }) filter?: FilterUserInput) {
+    return this.usersService.findAll(filter);
   }
 
   @Query(() => User, { name: 'user' })
