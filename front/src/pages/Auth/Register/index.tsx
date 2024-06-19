a1e06f5886124ea548926ef2e3990da42cd08422
pviprey <viprey.pierre@gmail.com>
2024-06-19 12:46:24 +0200

    Auth(feature) Creating the Register page

---
+++ front/src/pages/Auth/Register/index.tsx
@@ -0,0 +1,13 @@
+import Auth from '../../../components/molecules/Auth';
+
+const Register = () => {
+	return (
+		<Auth 
+			title='Welcome to Skype.'
+			info='Create account'
+			buttonText='Register me in.'
+		/>
+	);
+};
+
+export default Register;
