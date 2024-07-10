f5f808fb01ed61bb26c6e409a7fb071ad8a761ed
pviprey <viprey.pierre@gmail.com>
2024-07-10 09:38:13 +0200

    feat(login) : WIP but adding the capacity to create and log in your account

--- front/src/pages/Register/index.tsx
+++ front/src/pages/Register/index.tsx
@@ -1,14 +1,59 @@
-import Auth from '../../components/templates/Auth';
+import Auth, { UserInfoProps } from '../../components/templates/Auth';
+import { FormEvent, useState } from 'react'
+import { useRegisterMutation } from '../../generated/graphql';
+import { useAppDispatch } from '../../hooks/reduxHooks';
+import { useNavigate } from 'react-router';
 
 const Register = () => {
+	const [userInfo, setUserInfo] = useState<UserInfoProps>({name: '', password: ''});
+	const [register] = useRegisterMutation()
+	const dispatch = useAppDispatch()
+	const navigate = useNavigate()
+
+	const handleChangeUserInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target
+		setUserInfo({
+		  ...userInfo,
+		  [name]: value,
+		})
+	};
+
+	const handleSubmit = async (event: FormEvent) => {
+		event.preventDefault()
+		const username = userInfo.name
+		const password = userInfo.password
+
+		try {
+			if (!username || !password) {
+				return
+			}
+
+			const { data } = await register({
+				variables: { input: { username, password } },
+			  })
+			  if (data?.signup.username) {
+				navigate('/login')
+			  } else {
+				// setMsgError('An error occured, please try again later')
+			  }
+			} catch (e) {
+			  console.error(e)
+			//   setMsgError('An error occured')
+			}
+		}
+
 	return (
-		<Auth 
-			title='Welcome to Skype'
-			info='Create account'
-			buttonText='Register me'
-			redirectionLink='/login'
-			redirectionText='Sign in'
-		/>
+		<form onSubmit={handleSubmit}>
+			<Auth 
+				title='Welcome to Skype'
+				info='Create account'
+				buttonText='Register me'
+				redirectionLink='/login'
+				redirectionText='Sign in'
+				data={userInfo}
+				onChange={handleChangeUserInfo}
+			/>
+		</form>
 	);
 };
 
--- front/src/providers/RouterProvider.tsx
+++ front/src/providers/RouterProvider.tsx
@@ -8,42 +8,11 @@ import Homepage from '../pages/Homepage'
 import Chat from '../pages/Chat'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
-import { useAppDispatch, useAppSelector } from '../hooks/reduxHooks'
+import { useAppSelector } from '../hooks/reduxHooks'
 import NotFound from '../pages/NotFound'
-import { setLoggedUser } from '../features/userConnected'
 import AddContact from '../pages/AddContact'
 import Pagelayout from '../components/templates/PageLayout'
 
-/* export const router = createBrowserRouter([
-  {
-    path: ROUTES.HOME,
-    element: <Homepage />,
-  },
-  {
-    path: ROUTES.LOGIN,
-    element: <Login />,
-  },
-  {
-    path: ROUTES.REGISTER,
-    element: <Register />,
-  },
-  {
-    path: ROUTES.CHAT,
-    element: <Chat />,
-  },
-  {
-    path: ROUTES.ADD_CONTACT,
-    element: <AddContact />,
-  },
-  {
-    path: ROUTES.NOT_FOUND,
-    element: <NotFound />,
-  },
-  {
-    path: '*',
-    element: <Navigate to="/404" replace={true} />,
-  },
-]) */
 
 export const router = createBrowserRouter([
   {
@@ -90,25 +59,11 @@ export const authRouter = createBrowserRouter([
 ])
 
 const ActualRouter = () => {
-  const dispatch = useAppDispatch()
   const token = useAppSelector((state) => state.user)
-
-  dispatch(
-    setLoggedUser({
-      token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwic3ViIjoxLCJpYXQiOjE3MjA1MzEzODYsImV4cCI6MTcyMDUzNDk4Nn0.zF-vgFh5SZzUVMdgeb2BBIrSab3RK-7fyka_rRL92m8',
-      user: {
-        id: 1,
-        username: 'test',
-        avatar: 'test',
-      },
-    })
-  )
 
   return (
     <RouterProvider
       router={token.token ? router : authRouter}
-      /* fallbackElement={<BigSpinner />} */
     />
   )
 }
